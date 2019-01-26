8ba5ecba1ddb33c20ad61dc176e05adc13b72bf4
theredfish <did.julian@gmail.com>
2019-01-26 18:03:00 +0100

    Fix menu and add managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public MenuManager menuManager;
+
+    void Start()
+    {
+        menuManager.LoadMainMenu();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
